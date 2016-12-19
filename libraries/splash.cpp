e41ca8cc58d11ac82d791a624d48d46e837eaf15
FrimFrim of the clan Blue <tds1842@gmail.com>
2016-12-18 17:58:14 -0700

    Create splash.cpp

---
+++ libraries/splash.cpp
@@ -0,0 +1,31 @@
+
+void generate_splash(void)
+{
+
+	printf("            _            \r\n");
+	printf("           ---           \r\n");
+	printf("   ,_______|_|_______,   \r\n");
+	printf("  ---------------------  \r\n");
+	printf("  |                   |  \r\n");
+	printf(" ----------------------- \r\n");
+	printf(" || Police ---  Box   || \r\n");
+	printf(" ----------------------- \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  | | |  |  | | |  |  \r\n");
+	printf("  |  | | |  |  | | |  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  |---| ||| |   |  |  \r\n");
+	printf("  |  |-  |  |  |   |  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  |   |  |  |   |  |  \r\n");
+	printf("  |  |   |  |  |   |  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf("  |  |   |  |  |   |  |  \r\n");
+	printf("  |  |   |  |  |   |  |  \r\n");
+	printf("  |  -----  |  -----  |  \r\n");
+	printf(",-----------------------,\r\n");
+	printf("!-----------------------!\r\n");
+}
