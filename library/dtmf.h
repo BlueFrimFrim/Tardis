7ce769633186d2793a932ba71e1bef66f416aa2e
BlueFrimFrim <tds1842@gmail.com>
2017-05-28 18:24:43 -0600

    TODO: Remove extern 'C' wrapers

---
+++ library/dtmf.h
@@ -0,0 +1,17 @@
+#ifndef DTMF_H
+#define DTMF_H
+
+#include "Arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif /* extern __cplusplus */
+#endif /* DTMF_H */
