55fb57eb2db15cc78303686d7e0d9f89aa50536b
BlueFrimFrim <tds1842@gmail.com>
2017-05-29 19:24:30 -0600

    added Visual Studio Arduino project

---
+++ Receive_AVR/Receive_AVR/Receive_AVR.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Receive_AVR.ino
+ Created:	5/29/2017 6:39:36 PM
+ Author:	Keegan-Dev
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
