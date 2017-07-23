04d1dcebec8f27850d30ae3395301eb3491d5f88
BlueFrimFrim <tds1842@gmail.com>
2017-07-22 21:52:15 -0600

    .

--- Receive_AVR/Receive_AVR/init.h
+++
@@ -1,8 +0,0 @@
-#ifndef _INIT_H
-#define _INIT_H
-
-#include "dtmf.h"
-
-void init(void);
-
-#endif
--- Receive_AVR/Receive_AVR/irq.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Arduino.h"
-
-struct irq_data {
-	unsigned int irq;
-};
-
-extern void irq_event(void);
