f29e8437a4fdabd45ed2038e784e14f2f4573662
BlueFrimFrim <tds1842@gmail.com>
2017-12-30 00:58:48 -0700

    .

--- tardis_toneSIM/tardis.h
+++ tardis_toneSIM/tardis.h
@@ -7,14 +7,14 @@
 #include "dtmf.h"
 
 
-const byte D0 = 7;
-const byte D1 = 11;
-const byte D2 = 8;
-const byte D3 = 10;
+const byte D0 = 8;
+const byte D1 = 7;
+const byte D2 = 6;
+const byte D3 = 5;
 const byte IRQ_NOT = 12;
-const byte RS0 = 6;
-const byte RW = 5;
-const byte CS_NOT = 13;
+const byte RS0 = 4;
+const byte RW = 3;
+const byte CS_NOT = 2;
 
 const byte WRITE = 0;
 const byte READ = 1;
